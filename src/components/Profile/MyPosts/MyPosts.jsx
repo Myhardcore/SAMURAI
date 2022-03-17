10a63df141578468b0a8a5d178dfb4c89e8caf81
Myhardcore <nik-vna@mail.ru>
2022-03-17 21:48:46 +0300

    14

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,13 +2,10 @@ import React from "react";
 import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
 
-const MyPosts = () => {
-    let postsData = [
-        {id: 1, message: 'Hi,how are you doing?', likesCount: 11},
-        {id: 2, message: 'This is my first post', likesCount: 5},
-    ];
+const MyPosts = props => {
 
-    let postsElements = postsData.map(post => <Post message={post.message} count={post.likesCount} key={post.id}/>)
+
+    let postsElements = props.data.map(post => <Post message={post.message} count={post.likesCount} key={post.id}/>)
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,11 +3,14 @@ import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
-    
+    let postsData = [
+        {id: 1, message: 'Hi,how are you doing?', likesCount: 11},
+        {id: 2, message: 'This is my first post', likesCount: 5},
+    ];
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts data={postsData}/>
         </div>
     )
 }
